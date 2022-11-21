f14bc3e8b9e42de88dfac7cb5cd58de0e36d9648
leah wanjiku <leagm22@gmail.com>
2022-11-21 21:53:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName= 'Scuber';
+companyName == 'Scuber'; 
+mostProfitableNeighborhood: 'Chelsea';
+let mostProfitableNeighborhood ='Chelsea';
+let companyCeo = 'susan smith';
+companyCeo = 'Susan Smith';
+
+
+
